cae9361d20f9fa19d8002776387783be6d059112
abneren <58280300+abneren@users.noreply.github.com>
2020-10-25 19:33:12 +0800

    Add files via upload

---
+++ Personnel.java
@@ -0,0 +1,14 @@
+package text3;
+
+public class Personnel {
+	int number;
+    String name;
+    char sex;
+    Course a;
+    public Personnel(int number, String name, char sex) {
+        this.number = number;
+        this.name = name;
+        this.sex = sex;
+
+    }
+}
